80d4588fe4cf9359da82016b7a3d59675f286d62
pratimjyotig-wq <245566135+pratimjyotig-wq@users.noreply.github.com>
2026-08-15 09:57:44 +0530

    Refactor button interactions and add slides

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
-const question = document.getElementById("question");
-const message = document.getElementById("message");
+
+const slide1 = document.getElementById("slide1");
+const slide2 = document.getElementById("slide2");
+
 
 yesBtn.addEventListener("click", function () {
-    question.innerHTML = "YAYYYY! 🥰❤️";
-    message.innerHTML = "I knew you would say yes! 💕";
 
-    yesBtn.style.display = "none";
-    noBtn.style.display = "none";
+    slide1.classList.add("hidden");
+
+    slide2.classList.remove("hidden");
+
 });
 
-noBtn.addEventListener("mouseover", function () {
-    const maxX = window.innerWidth - noBtn.offsetWidth - 30;
-    const maxY = window.innerHeight - noBtn.offsetHeight - 30;
 
-    const x = Math.random() * maxX;
-    const y = Math.random() * maxY;
+noBtn.addEventListener("mouseover", function () {
+
+    const x = Math.random() * (window.innerWidth - 150);
+    const y = Math.random() * (window.innerHeight - 100);
 
     noBtn.style.position = "fixed";
     noBtn.style.left = x + "px";
     noBtn.style.top = y + "px";
+
 });
