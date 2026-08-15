388715ee22341353db622dc83e1f13f58066c2c9
pratimjyotig-wq <245566135+pratimjyotig-wq@users.noreply.github.com>
2026-08-15 09:51:52 +0530

    Improve yes and no button interactions
    Refactor yesBtn and noBtn event listeners for better functionality.

--- Script.js
+++ Script.js
@@ -1,31 +1,24 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
-
 const question = document.getElementById("question");
 const message = document.getElementById("message");
 
-
-// YES BUTTON
 yesBtn.addEventListener("click", function () {
-
-    question.innerHTML = "YAYYY! 🥰❤️";
-
-    message.innerHTML =
-        "I knew you would say yes! 💕";
+    question.innerHTML = "YAYYYY! 🥰❤️";
+    message.innerHTML = "I knew you would say yes! 💕";
 
     yesBtn.style.display = "none";
     noBtn.style.display = "none";
-
 });
 
-
-// NO BUTTON
 noBtn.addEventListener("mouseover", function () {
+    const maxX = window.innerWidth - noBtn.offsetWidth - 30;
+    const maxY = window.innerHeight - noBtn.offsetHeight - 30;
 
-    const x = Math.random() * 250 - 125;
-    const y = Math.random() * 200 - 100;
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
 
-    noBtn.style.transform =
-        `translate(${x}px, ${y}px)`;
-
+    noBtn.style.position = "fixed";
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
 });
